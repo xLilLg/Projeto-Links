d75b902ed9f4adc4c423f102359b8ed11877d99d
Luiz <luizguilhermeluzroberto@gmail.com>
2024-02-19 21:14:41 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Lg no espelho com filtro escuro")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Lg, óculos, cavanhaque, bigode, camisa preta e fundo natureza."
+    )
+  }
+}
